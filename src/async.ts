a37820fb43e2eef896412ae9ffe4076b1fc31ef5
Chet Corcos <ccorcos@gmail.com>
2019-10-20 23:03:10 -0700

    Works, but not idea.

--- src/async.ts
+++ src/async.ts
@@ -1,8 +1,10 @@
-// function chain<A extends keyof M, B, M extends { [key: string]: any }>(
-// 	args: [() => A, (arg: M[A]) => B]
-// ) {
-// 	return args
-// }
+/*
+
+- How does Promise.all work?
+	Create a DbBatch command!
+- How do we avoid the pyramid of doom?
+
+*/
 
 class IO<I, O, N> {
 	constructor(public input: I, public then: (o: O) => N) {}
@@ -11,28 +13,31 @@ class IO<I, O, N> {
 	public last: N extends IO<any, any, any> ? N["last"] : N
 }
 
-class DbGet<N> extends IO<{ type: "get"; id: string }, number, N> {}
-class DbSet<N> extends IO<
-	{ type: "set"; id: string; value: number },
-	void,
-	N
-> {}
+class DbGet<N> extends IO<{ type: "get"; id: string }, number, N> {
+	constructor(id: string, then: (n: number) => N) {
+		super({ type: "get", id }, then)
+	}
+}
 
-const x = new DbGet({ type: "get", id: "123" }, n => {
-	return new DbSet({ type: "set", id: "123", value: n + 1 }, () => {
+class DbSet<N> extends IO<{ type: "set"; id: string; value: number }, void, N> {
+	constructor(id: string, value: number, then: () => N) {
+		super({ type: "set", id, value }, then)
+	}
+}
+
+type Language<O = any> = DbGet<O> | DbSet<O>
+
+// function chain<A extends Language, B extends Language>(
+// 	args: [A, (arg: A["last"]) => B]
+// ): B["last"] {
+// 	return {} as any
+// }
+
+const x = new DbGet("123", n => {
+	return new DbSet("123", n + 1, () => {
 		return true
 	})
 })
-
-type Language = DbGet<any> | DbSet<any>
-
-function isLanguage(l: any): l is Language {
-	return l instanceof DbGet || l instanceof DbSet
-}
-
-function evaluate<L extends IO<any, any, any>>(language: L): L["last"] {
-	return {} as any
-}
 
 function evaluateDb<L extends Language>(language: L): L["last"] {
 	while (true) {
@@ -46,38 +51,3 @@ function evaluateDb<L extends Language>(language: L): L["last"] {
 	}
 }
 const y2 = evaluateDb(x)
-
-// This works except for the final return
-const y = evaluate(x)
-
-// type Proc<A, B, C> = (a: A) => IO<B, C>
-// type Proc2<A, B, C, D, E> = [Proc<A, B, C>, Proc<C, D, E>]
-
-// type Procs<L extends IO<any, any>, A, B> = [(a: A) => Array<(arg: L["i"]) => L>]
-
-// function chain2<A, B, C>(args: [() => IO<A, B>, (arg: B) => C]) {
-// 	return args
-// }
-// const x = chain2([() => new DbGet({ type: "get", id: "a" }), n => false])
-
-// function evaluate(args: Array)
-
-// ChainResult<
-// 	| IO<{type: "get", id: string}, object>
-// 	| IO<{type: "set", id: string, value: any}, void>,
-// 	undefined,
-// 	void
-// >
-
-// chain([
-// 	() => {
-// 		return get(id)
-// 	},
-// 	(result: x) => {
-// 		return set(id, {...x, color: 1})
-// 	},
-// ])
-
-function chain3<L extends IO<any, any>, A, B>(
-	args: [<T extends L>(a: A) => T]
-) {}
